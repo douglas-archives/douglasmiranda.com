87cbf17a9ed0306dd9a48b90607876ab405b846b
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-09-26 18:16:28 -0400

    fixes e alguns detalhes para melhor funcionamento do filebrowser

--- labs/models.py
+++ labs/models.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from django.template.defaultfilters import slugify
+# from django.template.defaultfilters import slugify
 from filebrowser.fields import FileBrowseField
+from filebrowser.settings import ADMIN_THUMBNAIL
 from datetime import datetime
 
 
@@ -12,10 +13,11 @@ class Projeto(models.Model):
 	descricao = models.CharField('Descrição', max_length=140,
 	help_text=u"Uma breve descrição em 140 caracteres. Um tweet :)")
 	publicacao = models.DateTimeField('Publicação', default=datetime.now, blank=True)
-	# imagem = models.ImageField(upload_to='labs/projetos')
-	document = FileBrowseField("PDF", max_length=200, directory="documents/", extensions=[".png",".jpg"], blank=True, null=True)
+	imagem = FileBrowseField("Imagem", max_length=200, directory="labs/projetos/imagens/",
+							  extensions=[".png", ".jpg", ".jpeg", ".gif"], format='image', blank=True, null=True)
 	status = models.BooleanField('Publicado no site', default=True,
                                   help_text='Se você marcar esta opção, o projeto ficará disponível para visualização no site.')
+
 
 	def __unicode__(self):
 		return self.titulo
@@ -25,6 +27,13 @@ class Projeto(models.Model):
 	# 		self.slug = slugify(self.titulo)
 	# 	super(Projeto, self).save(*args, **kwargs)
 
+	def image_thumbnail(self, obj):
+	    if obj.image and obj.image.filetype == "Image":
+	        return '<img src="%s" />' % obj.image.version_generate(ADMIN_THUMBNAIL).url
+	    else:
+	        return ""
+	image_thumbnail.allow_tags = True
+	image_thumbnail.short_description = "Thumbnail"
 	class Meta:
 		ordering = ['-publicacao']
 		get_latest_by = 'publicacao'
