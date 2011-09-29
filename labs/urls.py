a8043fbb3921f5b8df9f4727ab2cca06c0d92fc6
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-09-29 14:13:02 -0400

    passando a usar generic views para os projetos

--- labs/urls.py
+++ labs/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls.defaults import patterns, include, url
-from django.views.generic import ListView
-from labs.models import Projeto
+from django.conf.urls.defaults import patterns, url
+from labs.views import HomeListView
 
 
 urlpatterns = patterns('',
-    url(r'^projetos/',  ListView.as_view(model=Projeto,
-									  template_name = 'projetos.html',
-									  queryset=Projeto.objects.all(),
-									  context_object_name='ultimos_projetos'), name="projetos"),
+	url(r'^projetos/', HomeListView.as_view(), name='projetos'),
+    # url(r'^projetos/',  ListView.as_view(model=Projeto,
+				# 					  template_name = 'projetos.html',
+				# 					  queryset=Projeto.objects.all(),
+				# 					  context_object_name='ultimos_projetos'), name="projetos"),
 )
--- labs/views.py
+++ labs/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.views.generic import ListView
+from labs.models import Projeto
+
+class HomeListView(ListView):
+	context_object_name = 'ultimos_projetos'
+	model = Projeto
+	template_name = 'labs/projetos.html'
+	query_set = Projeto.objects.all()
