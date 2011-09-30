8648b3de32994f0892879ea39af4554333ccefd3
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-09-29 22:54:00 -0400

    habilitando de fato HomeListView

--- home/views.py
+++ home/views.py
@@ -1,12 +1,12 @@
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView
 from blog.models import Artigo
 
-# class HomeListView(ListView):
-# 	model = Artigo
-# 	template_name = 'home.html'
+class HomeListView(ListView):
+	model = Artigo
+	template_name = 'home.html'
 
-# 	def get_context_data(self, **kwargs):
-# 		context = super(HomeListView, self).get_context_data(**kwargs)
-# 		context['artigo_em_destaque'] = Artigo.objects.get_publicados().filter(principal=True).latest()
-# 		context['ultimos_artigos'] = Artigo.objects.get_publicados().exclude(pk=context['artigo_em_destaque'].pk)
-# 		return context
+	def get_context_data(self, **kwargs):
+		context = super(HomeListView, self).get_context_data(**kwargs)
+		context['artigo_em_destaque'] = Artigo.objects.get_publicados().filter(principal=True).latest()
+		context['ultimos_artigos'] = Artigo.objects.get_publicados().exclude(pk=context['artigo_em_destaque'].pk)
+		return context
