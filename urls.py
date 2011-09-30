a7f73de8277af86bb10320c7b02af39da87d199f
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-09-29 22:52:31 -0400

    refatorando a home

--- urls.py
+++ urls.py
@@ -3,21 +3,22 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from douglasmiranda import settings
-from blog.views import HomeListView
+from home.views import HomeListView
 # from blog.feeds import UltimosArtigos
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^$',  HomeListView.as_view(), name="inicial"),
-	# url(r'^$', 'blog.views.artigos_home', name="inicial"),
 	(r'^labs/', include('labs.urls')),
 	# (r'^blog/', include('blog.urls')),
 	# (r'^rss/(?P<url>.*)/$', 'django.contrib.syndication.views.feed',
 	# 	{'feed_dict': {'ultimos': UltimosArtigos}},
 	# ),
 	# (r'^artigo/(?P<artigo_id>\d+)/$', 'blog.views.artigo'),
+
 	(r'^media/(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+
 	(r'^grappelli/', include('grappelli.urls')),
 	(r'^admin/filebrowser/', include('filebrowser.urls')),
 	url(r'^admin/', include(admin.site.urls)),
