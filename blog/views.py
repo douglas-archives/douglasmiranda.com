cb67cb998084052bf5625f90e4dab7ab75627ea1
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-10-01 00:40:35 -0400

    adicionando os links para os artigos

--- blog/views.py
+++ blog/views.py
@@ -13,3 +13,8 @@ class ArtigoDetailView(DetailView):
 	template_name = 'blog/artigo.html'
 	context_object_name = 'artigo'
 	# queryset
+	def get_context_data(self, **kwargs):
+		context = super(ArtigoDetailView, self).get_context_data(**kwargs)
+		context['artigo'] = Artigo.objects.get_publicados().filter(principal=True).latest()
+		# context['artigos'] = Artigo.objects.get_publicados().exclude(pk=context['artigo_em_destaque'].pk)
+		return context
