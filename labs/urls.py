ce09eb2ddd2333815c279432dd97280be856926e
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-10-01 01:50:05 -0400

    removendo codigo desnecessario

--- labs/urls.py
+++ labs/urls.py
@@ -4,8 +4,4 @@ from labs.views import HomeListView
 
 urlpatterns = patterns('',
 	url(r'^projetos/', HomeListView.as_view(), name='labs-projetos'),
-    # url(r'^projetos/',  ListView.as_view(model=Projeto,
-				# 					  template_name = 'projetos.html',
-				# 					  queryset=Projeto.objects.all(),
-				# 					  context_object_name='ultimos_projetos'), name="projetos"),
 )
