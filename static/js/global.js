a4619b7aaeb5cc6acdeee70f413109e26e5d6470
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-10-04 12:52:23 -0400

    criando component search box :)

--- static/js/global.js
+++ static/js/global.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	// Efeito cursor nos links
 	cursor = '<span class="element-mouse-over-effect"> |</span>';
 	interval = null;
 	$('h3 a').hover(
@@ -20,4 +21,55 @@ $(document).ready(function(){
 			$(this).parent().find('span.element-mouse-over-effect').remove();
 		}
 	);
+
+	// search box
+	input_text = "#search-form input[type=text]"
+	input_submit = "#search-form input[type=submit]"
+	$(input_submit).hover(
+		function(){
+			$(this).addClass("hover");
+		},
+		function(){
+			$(this).removeClass("hover");
+		}
+	);
+	$(input_text).focus(function(){
+		$(this).addClass("active");
+		$(input_submit).stop().animate({
+			right: -95
+		}, 500, function() {
+			$(this).css('z-index', '1');
+		});
+	});
+	$(input_text).blur(function(){
+		$(this).removeClass("active");
+		if(!$(input_submit).hasClass("hover")){
+			$(input_submit).stop().css('z-index', '-1').animate({
+				right: '95'
+			}, 500);
+		}
+	});
+
+	// Placeholder
+	$('[placeholder]').focus(function() {
+	  var input = $(this);
+	  if (input.val() == input.attr('placeholder')) {
+		input.val('');
+		input.removeClass('placeholder');
+	  }
+	}).blur(function() {
+	  var input = $(this);
+	  if (input.val() == '' || input.val() == input.attr('placeholder')) {
+		input.addClass('placeholder');
+		input.val(input.attr('placeholder'));
+	  }
+	}).blur();
+	$('[placeholder]').parents('form').submit(function() {
+	  $(this).find('[placeholder]').each(function() {
+		var input = $(this);
+		if (input.val() == input.attr('placeholder')) {
+		  input.val('');
+		}
+	  })
+	});
 });
