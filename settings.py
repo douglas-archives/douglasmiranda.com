838c5ed7fb11a2ee27f23d7117978437dc4c62db
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-10-26 16:48:23 -0300

    tentando de outra forma

--- settings.py
+++ settings.py
@@ -63,7 +63,7 @@ MEDIA_URL = 'http://media.douglasmiranda.com/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/douglasmiranda/apps_wsgi/douglasmiranda/static/'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
