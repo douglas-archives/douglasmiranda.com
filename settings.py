2ff733ccddd63cea5e80169c086ba16112d2e005
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-10-29 13:14:43 -0300

    o like buttom do facebook precisava do endereço absoluto do artigo, então usei o context processor request para adicionar à url o domínio

--- settings.py
+++ settings.py
@@ -97,6 +97,15 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.app_directories.Loader',
     # 'django.template.loaders.eggs.Loader',
 )
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+)
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
