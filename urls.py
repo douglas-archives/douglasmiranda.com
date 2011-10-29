dc9f27545964e57da3fbaa852436cb2e7af7fa73
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-10-29 14:02:42 -0300

    adicionando alguns metadados

--- urls.py
+++ urls.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from home.views import HomeListView
 from blog.feeds import UltimosArtigos
+from douglasmiranda.settings import STATIC_URL
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+	(r'^favicon.png$', 'django.views.generic.simple.redirect_to', {'url': STATIC_URL+'images/favicon.png'}),
 	url(r'^$',  HomeListView.as_view(), name='projeto-inicial'),
 	url(r'^me/',  TemplateView.as_view(template_name='me.html'), name='projeto-me'),
 	(r'^labs/', include('labs.urls')),
