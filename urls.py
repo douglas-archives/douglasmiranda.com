b4f75effdcb981205b09eab1526bb0dec0359ccf
Douglas Miranda <douglasmirandasilva@gmail.com>
2012-05-13 02:20:59 -0400

    refatorando espaçamentos no urls.py

--- urls.py
+++ urls.py
@@ -6,22 +6,23 @@ from blog.feeds import UltimosArtigos
 from douglasmiranda.settings import STATIC_URL
 from django.conf import settings
 
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	(r'^robots.txt$', 'django.views.generic.simple.direct_to_template', {'template': 'robots.txt', 'mimetype': 'text/plain'}),
-	(r'^favicon.png$', 'django.views.generic.simple.redirect_to', {'url': STATIC_URL+'images/favicon.png'}),
-	url(r'^$',  HomeListView.as_view(), name='projeto-inicial'),
-	url(r'^me/',  TemplateView.as_view(template_name='me.html'), name='projeto-me'),
-	(r'^labs/', include('labs.urls')),
-	(r'^artigo/', include('blog.urls')),
-	(r'^rss/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': {'ultimos': UltimosArtigos}}),
+    (r'^robots.txt$', 'django.views.generic.simple.direct_to_template', {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+    (r'^favicon.png$', 'django.views.generic.simple.redirect_to', {'url': STATIC_URL + 'images/favicon.png'}),
+    url(r'^$',  HomeListView.as_view(), name='projeto-inicial'),
+    url(r'^me/',  TemplateView.as_view(template_name='me.html'), name='projeto-me'),
+    (r'^labs/', include('labs.urls')),
+    (r'^artigo/', include('blog.urls')),
+    (r'^rss/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': {'ultimos': UltimosArtigos}}),
 
 
-	# sirvo os arquivos estaticos do admin com o Django,
-	# por alguns problemas que ainda nao resolvi com o django-filebrowser
-	(r'^static/(.*)$', 'django.views.static.serve', {'document_root':settings.STATIC_ROOT}),
-	(r'^grappelli/', include('grappelli.urls')),
-	(r'^admin/filebrowser/', include('filebrowser.urls')),
-	url(r'^admin/', include(admin.site.urls)),
+    # sirvo os arquivos estaticos do admin com o Django,
+    # por alguns problemas que ainda nao resolvi com o django-filebrowser
+    (r'^static/(.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    (r'^grappelli/', include('grappelli.urls')),
+    (r'^admin/filebrowser/', include('filebrowser.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
