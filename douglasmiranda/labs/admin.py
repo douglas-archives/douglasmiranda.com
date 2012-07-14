22f94d2d2e27bfaad504eaec571ba418322c87e2
Douglas Miranda <douglasmirandasilva@gmail.com>
2012-07-14 03:10:42 -0400

    campo não mais utilizado devido a remoção do filebrowser

--- douglasmiranda/labs/admin.py
+++ douglasmiranda/labs/admin.py
@@ -3,7 +3,7 @@ from douglasmiranda.labs.models import Projeto
 
 
 class ProjetoAdmin(admin.ModelAdmin):
-    list_display = ['titulo', 'descricao', 'publicacao', 'status', 'image_thumbnail']
+    list_display = ['titulo', 'descricao', 'publicacao', 'status']
     list_filter = ['publicacao', 'status']
     search_fields = ['titulo', 'descricao']
     date_hierarchy = 'publicacao'
