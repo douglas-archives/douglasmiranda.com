2319bff2de90d121953bc7e112e93111147a0cc6
Douglas Miranda <douglasmirandasilva@gmail.com>
2013-01-23 03:07:53 -0300

    padronizando tuplas

--- douglasmiranda/blog/admin.py
+++ douglasmiranda/blog/admin.py
@@ -29,9 +29,9 @@ class ArtigoAdmin(admin.ModelAdmin):
     actions_on_bottom = True
     list_display = ('titulo', 'publicacao', 'status', 'principal')
     date_hierarchy = 'publicacao'
-    list_filter = ['publicacao', 'status']
+    list_filter = ('publicacao', 'status')
     list_editable = ('status', 'principal')
-    search_fields = ['titulo']
+    search_fields = ('titulo')
     prepopulated_fields = {"slug": ("titulo",)}
     radio_fields = {"status": admin.HORIZONTAL}
     form = ArtigoAdminForm
