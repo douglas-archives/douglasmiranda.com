25b4a72962a47e7e8633ccfbdc5db32055d96f61
Douglas Miranda <douglasmirandasilva@gmail.com>
2013-05-26 00:31:47 -0400

    production settings

---
+++ douglasmiranda/settings/prod.py
@@ -0,0 +1,16 @@
+from base import *
+from sensitive_data import DATABASES, SECRET_KEY
+
+STATIC_ROOT = '/home/douglas777/webapps/douglasmiranda_static_app/'
+MEDIA_ROOT = '/home/douglas777/webapps/douglasmiranda_media_app/'
+STATIC_URL = 'http://static.douglasmiranda.com/'
+MEDIA_URL = 'http://media.douglasmiranda.com/'
+
+COMPRESS_ENABLED = True
+STATICFILES_FINDERS += (
+    'compressor.finders.CompressorFinder',
+)
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter'
+]
